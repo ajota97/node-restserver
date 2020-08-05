109b00243cc79b417c38c5a04ce1300c39b816d4
Jose <jose8188258@gmail.com>
2020-08-04 20:54:30 -0400

    RestServer - usuario

---
+++ server/routes/usuario.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
+
+const Usuario = require('../models/usuario');
+
+
+//Obtener usuarios
+app.get('/usuario', function(req, res) {
+
+    let desde = Number(req.query.desde) || 0;
+    let limite = Number(req.query.limite) || 5;
+
+    Usuario.find({ estado: true }, 'nombre email role estado google img ')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.countDocuments({ estado: true }, (err, conteo) => {
+
+                //Devolver usuarios encontrados
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
+
+            });
+
+
+        }); //End Find
+
+});
+
+
+
+//Registrar nuevo usuario
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+    });
+});
+
+
+
+
+//Actualizar usuario
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    //Definiendo los campos que si se pueden actualizar
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: 'true',
+            usuario: usuarioDB
+        });
+
+    }); //End findAndUpdate
+
+});
+
+
+
+
+
+
+//Elimina un usuario de la BD
+app.delete('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    //Cambiamos el estado a false en vez de eliminarlo totalmente de la DB
+    Usuario.findByIdAndUpdate(id, { estado: false }, { new: true }, (err, userDelete) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+
+        if (userDelete) {
+            res.json({
+                ok: true,
+                usuario: userDelete
+            });
+        } else {
+            res.json({
+                ok: false,
+                error: { message: 'El usuario no existe' }
+            });
+        }
+
+
+    });
+
+});
+
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
@@ -13,39 +14,22 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+//Llamando a la ruta usuario
+app.use(require('./routes/usuario'));
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-});
 
-app.post('/usuario', function(req, res) {
+mongoose.connect(process.env.URLDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Base de datos Online');
+    });
 
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto: ', process.env.PORT);
